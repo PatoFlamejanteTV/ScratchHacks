b3135f9564adfe5c2cea1919017a3e0238d4b4da
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-08-12 15:55:27 -0300

    Create hacked2.js

---
+++ trolls/hacked2.js
@@ -0,0 +1,6 @@
+alert("Hacked by XxMarcos_")
+var links = document.getElementsByTagName("a");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].textContent = "HACKED LOL XDDDDXDDDDDDXDDDDDD";
+}
