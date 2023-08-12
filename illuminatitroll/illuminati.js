d762bd6b7abaa3419917f6e522531abb4e844f0a
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-08-12 15:34:24 -0300

    Update illuminati.js

--- illuminatitroll/illuminati.js
+++ illuminatitroll/illuminati.js
@@ -1,4 +1,6 @@
 setInterval(()=>{var imagees=document.getElementsByTagName("img");for(var ii=imagees.length-1;ii>0;ii--){imagees[ii].src="https://i.imgur.com/QM5K7mN.png";}},100);
+setInterval(()=>{var aa=document.getElementsByTagName("a");for(var iii=aa.length-1;iii>0;iii--){aa[iii].title="OMG ILUMINATI CONFIRMED?!?!?!?!?";}},100);
+
 // Create audio element
 var audioElement = document.createElement("audio");
 
@@ -6,7 +8,7 @@ var audioElement = document.createElement("audio");
 var audioElement = document.createElement("audio");
 
 // Define somethings
-audioElement.src = "https://download1501.mediafire.com/romcq747gfsgCnKaPZj0coIlgWAYu9uBpC_LmTjf0wiLyguOrluhTFD7bbRX_dcyFsmrnO8jaOw4viMsBp6eVL93fp0mTTqLAfPw5NF6m7uBcI3ATtKY_SRR87aq75-c7ghNARH0bCjxGeTyWpsMYRfxsTSjZ2TvP-wymeMuzzjRtwA/9grahtpnc8wbkvq/1.mp3";
+audioElement.src = "https://media1.vocaroo.com/mp3/1nrV7AAaiIz9";
 audioElement.controls = true;
 document.body.appendChild(audioElement);
 
