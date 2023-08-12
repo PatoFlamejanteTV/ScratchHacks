5ee2ab980a87b90905c9297b99ab3d85caeb1f79
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-08-12 16:06:29 -0300

    Create funni-omagaad.js

---
+++ trolls/funni-omagaad.js
@@ -0,0 +1,5 @@
+var image = document.getElementsByTagName("img");
+
+for (var i = 0; i < image.length; i++) {
+  image[i].src = "https://i1.sndcdn.com/artworks-XAVjL3tCPWjdrp2a-fjPL9g-t240x240.jpg";
+}
